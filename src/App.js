c7bf3bce4184e1efa071d440fed12f16577bce07
kovacsnandor <kovacsnandor@gmail.com>
2020-07-01 15:35:09 +0200

    Counter useCallback

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import './App.css';
 
 function App() {
     const [count, setCount] = useState(0);
     
-    const HandleClick = () => {
-        setCount(count + 1)
-    }
+    const HandleClick = useCallback(() => {
+        setCount(prevState => prevState + 1)
+    }, [setCount])
 
   return (
     <div className="App">
       <span>{count}</span>
       <button onClick={HandleClick}>Increment</button>
     </div>
-  );
+  )
 }
 
 export default App;
